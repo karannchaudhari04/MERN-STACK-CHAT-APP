5474ada3646ea380c143248e0b7fa56903077fb3
karannchaudhari04 <karanchaudhari34804@gmail.com>
2025-06-18 19:36:57 +0530

    initial commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,47 +2,39 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
-import path from "path";
-
 import { connectDB } from "./lib/db.js";
-
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
-const __dirname = path.resolve();
+const PORT = process.env.PORT || 5000;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 app.use(
   cors({
     origin: [
-      "http://localhost:5173", 
-      "https://mern-stack-chat-app-rosy.vercel.app"
+      "http://localhost:5173", // local dev
+      "https://mern-stack-chat-app-rosy.vercel.app", // Vercel frontend
     ],
     credentials: true,
   })
 );
 
-
+// API routes only
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+// Default route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
   connectDB();
 });
-
-
